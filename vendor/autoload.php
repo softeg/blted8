68cd88e479782fad26fa973184605cdb0b003022
Travis-CI <noreply@travis-ci.org>
2017-03-09 15:57:00 +0000

    Automated commit by Travis CI for Build 209394776

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit552c4b2fb9db9af1406067d726a75c31::getLoader();
+return ComposerAutoloaderInit2ec577f9c7bb0ec813dfff6bc919fed0::getLoader();
