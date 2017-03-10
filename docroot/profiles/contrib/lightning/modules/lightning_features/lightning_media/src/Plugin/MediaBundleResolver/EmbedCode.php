5d1f15da56b3d1968114d779ba156504fdc4e52f
Travis-CI <noreply@travis-ci.org>
2017-03-10 16:08:39 +0000

    Automated commit by Travis CI for Build 8.x.ad6c8e53bb6ff9296cc4aa12cbf30cd6f171fff9

---
+++ docroot/profiles/contrib/lightning/modules/lightning_features/lightning_media/src/Plugin/MediaBundleResolver/EmbedCode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\lightning_media\Plugin\MediaBundleResolver;
+
+use Drupal\lightning_media\BundleResolverBase;
+
+/**
+ * Bundle resolver for embed codes.
+ *
+ * @deprecated in Lightning 2.0.4 and will be removed in Lightning 3.x. Media
+ * type plugins should implement InputMatchInterface directly instead.
+ *
+ * @MediaBundleResolver(
+ *   id = "embed_code"
+ * )
+ */
+class EmbedCode extends BundleResolverBase {
+}
